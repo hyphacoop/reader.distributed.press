46039c0d1bb6ba9473bd82f2a40da3dc3187791a
Mauve Signweaver <RangerMauve@hotmail.com>
2024-02-29 00:51:33 -0500

    Implement activity fetching  and indexing

--- db.js
+++ db.js
@@ -1,7 +1,10 @@
 import { openDB } from './dependencies/idb/index.js'
 
+export const DEFAULT_DB = 'default'
 export const ACTORS_STORE = 'actors'
 export const NOTES_STORE = 'notes'
+export const ACTIVITIES_STORE = 'activities'
+
 export const ID_FIELD = 'id'
 export const URL_FIELD = 'url'
 export const CREATED_FIELD = 'created'
@@ -13,6 +16,13 @@ export const IN_REPLY_TO_FIELD = 'inReplyTo'
 export const TAG_NAMES_FIELD = 'tag_names'
 export const ATTRIBUTED_TO_FIELD = 'attributedTo'
 export const CONVERSATION_FIELD = 'conversation'
+export const ACTOR_FIELD = 'actor'
+
+export const PUBLISHED_SUFFIX = ', published'
+
+export const TYPE_CREATE = 'Create'
+export const TYPE_NOTE = 'Note'
+export const TYPE_DELETE = 'Delete'
 
 // TODO: When ingesting notes and actors, wrap any dates in `new Date()`
 // TODO: When ingesting notes add a "tag_names" field which is just the names of the tag
@@ -25,7 +35,7 @@ export class ActivityPubDB {
     this.fetch = fetch
   }
 
-  static async load (name, fetch = globalThis.fetch) {
+  static async load (name = DEFAULT_DB, fetch = globalThis.fetch) {
     const db = await openDB(name, 1, {
       upgrade
     })
@@ -33,19 +43,38 @@ export class ActivityPubDB {
     return new ActivityPubDB(db, fetch)
   }
 
+  async #get (url) {
+    if (url && typeof url === 'object') {
+      return url
+    }
+    // TODO: Signed fetch
+    const response = await this.fetch.call(globalThis, url, {
+      headers: {
+        Accept: 'application/json'
+      }
+    })
+    if (!response.ok) {
+      throw new Error(await response.text())
+    }
+
+    return await response.json()
+  }
+
   async getActor (url) {
-    // Try to load from db
-    // else try to ingest then load from db
-    const request = await this.fetch(url, { headers: { Accept: 'application/ld+json' } })
-    // Handle 404
-    const actor = await request.json()
-    this.db.add(ACTORS_STORE, actor)
+    // TODO: Try to load from cache
+    const actor = await this.#get(url)
+    this.db.put(ACTORS_STORE, actor)
     return this.db.get(ACTORS_STORE, actor.id)
   }
 
   async getNote (url) {
-    // Try to load from db
-    // Else try to ingest then load from db
+    try {
+      return this.db.get(NOTES_STORE, url)
+    } catch {
+      const note = await this.#get(url)
+      await this.ingestNote(note)
+      return note
+    }
   }
 
   async * searchNotes (query) {
@@ -53,28 +82,74 @@ export class ActivityPubDB {
     // Everything gets sorted by the `published` time.
     // We should find a way to allow for arrays of values and open multiple iterators
     // Kinda like this: https://git.sr.ht/~breatheoutbreathein/ushin-db/tree/master/item/USHINBase.js#L509
+
+    const tx = this.db.transaction(NOTES_STORE)
+
+    for await (const item of tx.store) {
+      yield item.value
+    }
   }
 
   async ingestActor (url) {
-    // Load actor and save to actors store
+    const actor = await this.getActor(url)
+    // TODO: What else can we ingest from an actor?
     // Ingest outbox as collection
+    const { outbox } = actor
+
+    // TODO: Use actor.id?
+    await this.ingestActivityCollection(outbox, url)
   }
 
-  async ingestCollection (url, verifyAttributed = '') {
-    // Load outbox as async iterator, handle extra pages
-    // Go through each activity and if it's a note, ingest or delete it
+  async * iterateCollection (url) {
+    const collection = await this.#get(url)
+    // TODO: handle paging and skiping
+    const items = collection.items || collection.orderedItems
+
+    if (!items) throw new Error(`Unable to find items at ${url}`)
+    for await (const item of items) {
+      if (typeof item === 'string') {
+        const data = await this.#get(item)
+        yield data
+      } else yield item
+    }
   }
 
-  async ingestNote (url) {
-  // Load by url
+  async ingestActivityCollection (url, verifyAttributed = '') {
+    for await (const activity of this.iterateCollection(url)) {
+      if (verifyAttributed && activity.actor !== verifyAttributed) {
+        throw new Error(`Collection contained activity not attributed to ${verifyAttributed} at ${url} in activity ${activity.id}`)
+      }
+      await this.ingestActivity(activity)
+    }
+  }
+
+  async ingestActivity (activity) {
+    activity.published = new Date(activity.published)
+    console.log(activity)
+    await this.db.put(ACTIVITIES_STORE, activity)
+    if (activity.type === TYPE_CREATE) {
+      const object = await this.#get(activity.object)
+      if (object.type === TYPE_NOTE) {
+        console.log(object)
+        await this.ingestNote(object)
+      }
+    } else if (activity.type === TYPE_DELETE) {
+      await this.deleteNote(activity.object)
+    }
+  }
+
+  async ingestNote (note) {
   // Convert needed fields to date
-  // Add tag_names field
-    // Add to notes store
-    // Loop through replies (if possible) and ingest them
+    note.published = new Date(note.published)
+    // Add tag_names field
+    note.tag_names = (note.tags || []).map(({ name }) => name)
+    this.db.put(NOTES_STORE, note)
+    // TODO: Loop through replies
   }
 
   async deleteNote (url) {
     // delete note using the url as the `id` from the notes store
+    this.db.delete(NOTES_STORE, url)
   }
 }
 
@@ -93,19 +168,25 @@ function upgrade (db) {
     autoIncrement: false
   })
 
-  addRegularIndex(PUBLISHED_FIELD)
-  addRegularIndex(TO_FIELD)
-  addRegularIndex(URL_FIELD)
-  addSortedIndex(TAG_NAMES_FIELD, { multiEntry: true })
-  addSortedIndex(IN_REPLY_TO_FIELD)
-  addSortedIndex(ATTRIBUTED_TO_FIELD)
-  addSortedIndex(CONVERSATION_FIELD)
-  addSortedIndex(TO_FIELD)
+  addRegularIndex(notes, TO_FIELD)
+  addRegularIndex(notes, URL_FIELD)
+  addRegularIndex(notes, TAG_NAMES_FIELD, { multiEntry: true })
+  addSortedIndex(notes, IN_REPLY_TO_FIELD)
+  addSortedIndex(notes, ATTRIBUTED_TO_FIELD)
+  addSortedIndex(notes, CONVERSATION_FIELD)
+  addSortedIndex(notes, TO_FIELD)
 
-  function addRegularIndex (field, options = {}) {
-    notes.createIndex(field, field, options)
+  const activities = db.createObjectStore(ACTIVITIES_STORE, {
+    keyPath: 'id',
+    autoIncrement: false
+  })
+  addSortedIndex(activities, ACTOR_FIELD)
+  addSortedIndex(activities, TO_FIELD)
+
+  function addRegularIndex (store, field, options = {}) {
+    store.createIndex(field, field, options)
   }
-  function addSortedIndex (field, options = {}) {
-    notes.createIndex(field, [field, PUBLISHED_FIELD], options)
+  function addSortedIndex (store, field, options = {}) {
+    store.createIndex(field + ', published', [field, PUBLISHED_FIELD], options)
   }
 }
