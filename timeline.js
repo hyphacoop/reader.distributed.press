2a8ecd671c45112bbee8039f3becc95706bcaa7b
Akhilesh Thite <akhithite@gmail.com>
2024-04-25 00:41:00 +0530

    feat: add distributed.press fedi account to default followed list

--- timeline.js
+++ timeline.js
@@ -28,6 +28,7 @@ class ReaderTimeline extends HTMLElement {
   async initializeDefaultFollowedActors () {
     const defaultActors = [
       'https://social.distributed.press/v1/@announcements@social.distributed.press/',
+      'https://distributed.press/about.jsonld',
       'https://hypha.coop/about.jsonld',
       'https://sutty.nl/about.jsonld'
       // "https://akhilesh.sutty.nl/about.jsonld",
