b17f3f3169b14a2b60d26c39f69846eb118428f3
Akhilesh Thite <akhithite@gmail.com>
2024-04-26 23:26:58 +0530

    refactor: load sidebar using top-level await and remove redundant check

--- sidebar.js
+++ sidebar.js
@@ -1,17 +1,10 @@
 let sidebarTemplateContent
 
-async function loadSidebarTemplate () {
-  const response = await fetch('./sidebar.html')
-  const text = await response.text()
-  const parser = new DOMParser()
-  const doc = parser.parseFromString(text, 'text/html')
-  sidebarTemplateContent = doc.getElementById('sidebar-template').content
-
-  customElements.define('sidebar-nav', SidebarNav)
-}
-
-// Call the function to load the sidebar template immediately
-loadSidebarTemplate()
+const response = await fetch('./sidebar.html')
+const text = await response.text()
+const parser = new DOMParser()
+const doc = parser.parseFromString(text, 'text/html')
+sidebarTemplateContent = doc.getElementById('sidebar-template').content
 
 class SidebarNav extends HTMLElement {
   constructor () {
@@ -21,17 +14,14 @@ class SidebarNav extends HTMLElement {
   }
 
   initSidebar () {
-    // Use the pre-loaded sidebar template content
-    if (sidebarTemplateContent) {
-      const instance = sidebarTemplateContent.cloneNode(true)
-      this.shadowRoot.appendChild(instance)
+    const instance = sidebarTemplateContent.cloneNode(true)
+    this.shadowRoot.appendChild(instance)
 
-      // Attach the sidebar.css styles
-      const style = document.createElement('style')
-      style.textContent = '@import url("./sidebar.css");'
-      this.shadowRoot.appendChild(style)
-    } else {
-      console.error('Sidebar template has not been loaded yet')
-    }
+    // Attach the sidebar.css styles
+    const style = document.createElement('style')
+    style.textContent = '@import url("./sidebar.css");'
+    this.shadowRoot.appendChild(style)
   }
 }
+
+customElements.define('sidebar-nav', SidebarNav)
