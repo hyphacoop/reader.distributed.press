c20c9dbd10262aafbfc8d63d9722be0785b1811a
Akhilesh Thite <akhithite@gmail.com>
2024-05-03 20:18:13 +0530

    feat: load default followed actors from p2p protocols

--- timeline.js
+++ timeline.js
@@ -28,8 +28,8 @@ class ReaderTimeline extends HTMLElement {
   async initializeDefaultFollowedActors () {
     const defaultActors = [
       'https://social.distributed.press/v1/@announcements@social.distributed.press/',
-      'https://distributed.press/about.jsonld',
-      'https://hypha.coop/about.jsonld',
+      'ipns://distributed.press/about.jsonld',
+      'hyper://hypha.coop/about.jsonld',
       'https://sutty.nl/about.jsonld'
       // "https://akhilesh.sutty.nl/about.jsonld",
       // "https://staticpub.mauve.moe/about.jsonld",
